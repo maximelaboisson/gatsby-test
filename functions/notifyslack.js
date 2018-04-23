3f14447a48a448732a9fff2ff7337a102008e613
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-23 11:38:22 -0400

    asda

--- functions/notifyslack.js
+++ functions/notifyslack.js
@@ -1,4 +1,21 @@
+const fetch = require('node-fetch');
+
 exports.handler = function(event, context, callback) {
     var webhook = 'https://hooks.slack.com/services/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';
-    console.log(event.body);
+    
+    var message = `${event.body.data.name}(${event.body.data.email}): ${event.body.data.message}`;
+
+    fetch(webhook, {
+        body: JSON.stringify(message),
+        cache: 'no-cache',
+        headers: {
+          'content-type': 'application/json',
+          "Access-Control-Allow-Origin" : "*"
+        },
+        method: 'POST',
+        redirect: 'follow',
+        referrer: 'no-referrer',
+      })
+      .then(response => response.json())
+      .then(x => console.log(x));
 }
