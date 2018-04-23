c4e055321fe675d065f4fa59c4b13d3369e0ca9a
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-22 21:29:33 -0400

    Fixed fetch...

--- functions/fetchreviews.js
+++ functions/fetchreviews.js
@@ -1,27 +1,39 @@
 const fetch = require('node-fetch');
 
 exports.handler = function(event, context, callback) {
-    var id = event.queryStringParameters.id;
-    var token = 'eb34003b40f217432461bc6a272d8b5582ccdf15c14597f12754f3029a55dfbb'
+    try {
+        var id = event.queryStringParameters.id;
+        var token = 'eb34003b40f217432461bc6a272d8b5582ccdf15c14597f12754f3029a55dfbb'
+    
+        if(id == undefined){
+            callback('A product id must be specified.', {
+                statusCode: 500
+            })
+        }
+    
+        fetch(`https://api.netlify.com/api/v1/forms?access_token=${token}`)
+            .then(x => x.json())
+            .then(x => {
+                var form = x.filter(y => y.name == `product-${id}`)[0]
 
-    if(!!id){
-        callback('A product id must be specified.')
+                if(form == null){
+                    callback('The product id must be valid.')
+                }
+    
+                fetch(`https://api.netlify.com/api/v1/forms/${form.id}/submissions?access_token=${token}`)
+                    .then(x => x.json())
+                    .then(x => {
+                        callback(null, {
+                            statusCode: 200,
+                            body: JSON.stringify(x)
+                        })
+                    })
+            })
     }
-
-    fetch(`https://api.netlify.com/api/v1/forms?access_token=${token}`)
-        .then(x => x.json())
-        .then(x => {
-            var form = x.filter(y => y.id == id)[0]
-
-            if(form == null){
-                callback('The product id must be valid.')
-            }
-
-            fetch(`https://api.netlify.com/api/v1/forms/${form.id}/submissions?access_token=${token}`)
-                .then(x => x.json())
-                .then(x => callback(null, {
-                    statusCode: 200,
-                    body: x
-                }))
+    catch (error){
+        callback('Something bad happened.', {
+            statusCode: 500,
+            body: error
         })
+    }
 }
---
+++ functions/gatsby-config.js
@@ -0,0 +1,89 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(__dirname) {
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter'
+  },
+  plugins: ['gatsby-plugin-react-helmet', {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/src/data/products`,
+      name: "markdown-pages"
+    }
+  }, `gatsby-transformer-remark`]
+};
+/* WEBPACK VAR INJECTION */}.call(exports, "/"))
+
+/***/ })
+/******/ ])));
