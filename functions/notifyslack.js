e45da5d311c9f6208282fdbb979dc449531c8619
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-23 13:53:25 -0400

    asdadasda

--- functions/notifyslack.js
+++ functions/notifyslack.js
@@ -1,6 +1,5 @@
-//const fetch = require('node-fetch').config();
-
 var http = require("http");
+var querystring = require('querystring');
 
 exports.handler = function(event, context, callback) {
     var body = JSON.parse(event.body);
@@ -9,25 +8,36 @@ exports.handler = function(event, context, callback) {
         var data = body.data;
         var message = `${data.name}(${data.email}): ${data.message}`;
 
+        var postData = querystring.stringify({
+            text: message
+        });
+
         var options = {
             hostname: 'www.hooks.slack.com',
-            port: '443',
             path: '/services/T0253KADL/BAB9445T5/IWjaMiSOjHIdf8tvq2D9oGPe',
             method: 'POST',
-            headers: {
-                "content-type": "application/json",
+            headers: {        
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': postData.length,
                 "Access-Control-Allow-Origin" : "*"
             }
         };
     
         var req = http.request(options, function(res) {
             res.setEncoding('utf8');
+            
             res.on('data', function (body) {
                 console.log('Body: ' + body);
             });
+            
+            res.on('end', function () {
+                callback(null, {
+                    statusCode: 200
+                })
+            });
         });
-        
-        req.write(`{"text": "${message}"}`);
+
+        req.write(postData);
         req.end();    
     }
 }
