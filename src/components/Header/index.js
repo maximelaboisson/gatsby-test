0d846728dc8c23e5f7bee5afd5298d66b0a58c9f
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-24 15:07:55 -0400

    asdadas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,37 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
+const netlifyIdentity = require("netlify-identity-widget");
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
+export default class Header extends React.Component {  
+  componentDidMount(){    
+    netlifyIdentity.init({
+      container: "#netlify-modal"
+    });
+  }
+  
+  render(){
+    return (
     <div
       style={{
-        display: 'flex',
-        margin: '0 auto',
-        maxWidth: '960px',
-        padding: '1.45rem 1.0875rem',
-        justifyContent: 'space-between'
+        background: 'rebeccapurple',
+        marginBottom: '1.45rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-      
-      <div data-netlify-identity-menu></div>
-    </div>
-  </div>
-)
-
-export default Header
+      <div
+        style={{
+          display: 'flex',
+          margin: '0 auto',
+          maxWidth: '960px',
+          padding: '1.45rem 1.0875rem',
+          justifyContent: 'space-between'
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            Gatsby
+          </Link>
+        </h1>
+        <div id="netlify-modal"></div>
+      </div>
+    </div>)
+  }
+}
