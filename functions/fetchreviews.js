8d35b4b66a961d47b067e2fa34d931b1b566729c
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-24 00:07:20 -0400

    asdadas

--- functions/fetchreviews.js
+++ functions/fetchreviews.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const https = require('https');
 
 exports.handler = function(event, context, callback) {
     var id = event.queryStringParameters.id;
@@ -10,25 +10,67 @@ exports.handler = function(event, context, callback) {
         })
     }
 
-    fetch(`https://api.netlify.com/api/v1/forms?access_token=${token}`)
-        .then(x => x.json())
-        .then(x => {
-            var form = x.filter(y => y.name == `product-${id}`)[0]
+    var options = {
+        hostname: 'api.netlify.com/api/v1/',
+        port: 443,
+        path: '',
+        method: 'GET',
+        headers: {        
+            'Content-Type': 'application/json'
+        }
+    };
 
+    var queryToken = `access_token=${token}`;
+    var opts1 = Object.assign({}, { path: `/forms?${queryToken}`}, options);
+    
+    var req = https.request(opts1, function(res) {
+
+        res.setEncoding('utf8');
+        var body = "";
+
+        res.on("data", (data) => {
+            body += data;
+        });
+
+        res.on('end', function () {
+            body = JSON.parse(body);
+
+            var form = body.filter(x => x.name == `product-${id}`)[0];
+            var opts2 = Object.assign({}, { path: `/forms/${form.id}/submissions?${queryToken}`}, options);
             if(form == null){
                 callback('The product id must be valid.')
             }
 
-            fetch(`https://api.netlify.com/api/v1/forms/${form.id}/submissions?access_token=${token}`)
-                .then(x => x.json())
-                .then(x => {
+            var req2 = https.request(opts2, function(res2) {
+            
+                res2.setEncoding('utf8');         
+                var body2 = "";
+
+                res2.on("data", (data) => {
+                    body2 += data;
+                });
+
+                res.on('end', function () {
                     callback(null, {
                         statusCode: 200,
                         headers: {
                             "Access-Control-Allow-Origin" : "*"
                         },
-                        body: JSON.stringify(x)
+                        body: body2
                     })
-                })
-        })
+                });
+            });
+        });
+    });
+    
+    req.on('error', function (e) {
+        console.log('Problem with request:', e.message);
+    });
+
+    req.write(postData);
+    req.end();
+
+    callback(null, {
+        statusCode: 200
+    })  
 }
