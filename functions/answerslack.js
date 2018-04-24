92925fe8932159412efe2ca945bc3a0ba8fa0ee9
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2018-04-23 22:54:21 -0400

    asdadasdas

--- functions/answerslack.js
+++ functions/answerslack.js
@@ -1,6 +1,20 @@
 var https = require("https");
 var qs = require('querystring')
 
+function getURL(href) {
+    var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+    return match && {
+        href: href,
+        protocol: match[1],
+        host: match[2],
+        hostname: match[3],
+        port: match[4],
+        pathname: match[5],
+        search: match[6],
+        hash: match[7]
+    }
+}
+
 exports.handler = function(event, context, callback) {
     var json = JSON.parse(qs.parse(event.body).payload);
 
@@ -9,8 +23,7 @@ exports.handler = function(event, context, callback) {
         text: "The review has been approved!"
     });
 
-    var url = new URL(`www.${json.response_url
-        .replace(/(^\w+:|^)\/\//, '')}`);
+    var url = this.getURL(json.response_url);
 
     var options = {
         hostname: url.hostname,
